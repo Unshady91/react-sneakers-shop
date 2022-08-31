82187cd872b3bda50ced56eb219d4604dd4229a8
Azamat M <amoldashev@gmail.com>
2022-08-31 16:25:52 +0500

    Вносит разметку блока content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./css/style.scss";
 import { Header } from './components/header.jsx';
 import { Banner } from './components/banner.jsx';
+import { Content } from './components/content.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Content />
     </div>
   );
 }
---
+++ src/components/content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <section className="content container">
+      <h1 className="content__title">Все кроссовки</h1>
+    </section>
+  );
+}
+
+export { Content };
