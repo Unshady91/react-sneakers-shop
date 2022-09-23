16fbf083e0873a90c110a24be0901e1a024b0995
Azamat M <amoldashev@gmail.com>
2022-09-23 17:40:37 +0500

    Вносит правки

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -4,21 +4,42 @@ function Cart(props) {
   return (
     <div className="overlay">
       <div className="menu-cart">
-        <div className="inner">
-          <div className="menu-cart__top">
-            <h1 className="menu-cart__title">Ваша корзина</h1>
-            <button
-              className="button menu-cart__button"
-              type="button"
-              onClick={props.cartToggler}
-            />
-          </div>
+        <header className="menu-cart__header">
+          <h1 className="menu-cart__title">Ваша корзина</h1>
+          <button
+            className="button menu-cart__button"
+            type="button"
+            onClick={props.cartToggler}
+          />
+        </header>
+        <div className="menu-cart__content">
           <ul className="list menu-cart__list">
-            {
-            props.items.map((item, index) => <CartItem {...item} key={index} />)
-            }
+            {props.items.map((item, index) => (
+              <CartItem {...item} key={index} />
+            ))}
           </ul>
         </div>
+        <footer className="menu-cart__footer">
+          <div className="menu-order">
+            <div className="menu-order__content">
+              <div className="menu-order__top">
+                <p className="menu-order__text">Итого:</p>
+                <p className="menu-order__text menu-order__text--bold">
+                  21498&nbsp;руб.
+                </p>
+              </div>
+              <div className="menu-order__bottom">
+                <p className="menu-order__text">Налог 5%:</p>
+                <p className="menu-order__text menu-order__text--bold">
+                  1074&nbsp;руб.
+                </p>
+              </div>
+              <button type="submit" className="menu-order__button">
+                Оформить заказ
+              </button>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
